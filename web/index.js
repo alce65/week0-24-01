21fc8c91be87596e2ccf05ec7aba94c773fdad41
alce65 <alce65@hotmail.es>
2024-02-12 17:02:54 +0100

    Sample Web with JS

---
+++ web/index.js
@@ -0,0 +1,7 @@
+import { greeting } from "./sample.js";
+
+console.log("Hola desde un fichero JS!");
+
+// Ejecutando (llamándola) una función
+// importada de otro fichero JS
+greeting();
---
+++ web/sample.js
@@ -0,0 +1,4 @@
+// Creando una función
+export function greeting() {
+  console.log("Soy una función en un fichero JS!");
+}
